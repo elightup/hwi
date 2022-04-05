74c6a8cbfc64c6e3b15817bf68a228bc9b25bf28
tuyenlq94 <tuyenicolor@gmail.com>
2022-04-05 16:30:27 +0700

    what we do mobile

--- js/script.js
+++ js/script.js
@@ -268,6 +268,14 @@ jQuery( function ( $ ) {
 					breakpoint: 767,
 					settings: {
 						slidesToShow: 2,
+						slidesToScroll: 2,
+					}
+				},
+				{
+					breakpoint: 480,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
 					}
 				}
 			]
